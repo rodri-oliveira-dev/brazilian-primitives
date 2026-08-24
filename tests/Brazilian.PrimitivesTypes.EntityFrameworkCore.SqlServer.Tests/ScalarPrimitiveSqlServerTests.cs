28a732bbde2b2b876267e20de325a888543dcc2b
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-08-24 00:30:45 -0300

    style(efcore-sqlserver): format scalar mappings

--- tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/ScalarPrimitiveSqlServerTests.cs
+++ tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/ScalarPrimitiveSqlServerTests.cs
@@ -192,32 +192,52 @@ public sealed class ScalarPrimitiveSqlServerTests
             {
                 entity.ToTable("ScalarPrimitiveRecords");
                 entity.HasKey(record => record.Id);
-                entity.Property(record => record.Cpf).HasConversion(new CpfValueConverter());
-                entity.Property(record => record.Cnpj).HasConversion(new CnpjValueConverter());
-                entity.Property(record => record.CpfCnpj).HasConversion(new CpfCnpjValueConverter());
-                entity.Property(record => record.Cep).HasConversion(new CepValueConverter());
-                entity.Property(record => record.Email).HasConversion(new EmailValueConverter());
-                entity.Property(record => record.OptionalEmail).HasConversion(new EmailValueConverter());
-                entity.Property(record => record.MobilePhone).HasConversion(new MobilePhoneValueConverter());
-                entity.Property(record => record.LandlinePhone).HasConversion(new LandlinePhoneValueConverter());
-                entity.Property(record => record.TelefoneBrasileiro).HasConversion(new TelefoneBrasileiroValueConverter());
-                entity.Property(record => record.ChavePix).HasConversion(new ChavePixValueConverter());
-                entity.Property(record => record.Cnh).HasConversion(new CnhValueConverter());
-                entity.Property(record => record.Cns).HasConversion(new CnsValueConverter());
-                entity.Property(record => record.TituloEleitoral).HasConversion(new TituloEleitoralValueConverter());
-                entity.Property(record => record.Nit).HasConversion(new NitValueConverter());
-                entity.Property(record => record.PisPasep).HasConversion(new PisPasepValueConverter());
-                entity.Property(record => record.PlacaVeiculo).HasConversion(new PlacaVeiculoValueConverter());
-                entity.Property(record => record.Renavam).HasConversion(new RenavamValueConverter());
-                entity.Property(record => record.Ispb).HasConversion(new IspbValueConverter());
-                entity.Property(record => record.CodigoCompe).HasConversion(new CodigoCompeValueConverter());
+                entity.Property(record => record.Cpf)
+                    .HasConversion(new CpfValueConverter());
+                entity.Property(record => record.Cnpj)
+                    .HasConversion(new CnpjValueConverter());
+                entity.Property(record => record.CpfCnpj)
+                    .HasConversion(new CpfCnpjValueConverter());
+                entity.Property(record => record.Cep)
+                    .HasConversion(new CepValueConverter());
+                entity.Property(record => record.Email)
+                    .HasConversion(new EmailValueConverter());
+                entity.Property(record => record.OptionalEmail)
+                    .HasConversion(new EmailValueConverter());
+                entity.Property(record => record.MobilePhone)
+                    .HasConversion(new MobilePhoneValueConverter());
+                entity.Property(record => record.LandlinePhone)
+                    .HasConversion(new LandlinePhoneValueConverter());
+                entity.Property(record => record.TelefoneBrasileiro)
+                    .HasConversion(new TelefoneBrasileiroValueConverter());
+                entity.Property(record => record.ChavePix)
+                    .HasConversion(new ChavePixValueConverter());
+                entity.Property(record => record.Cnh)
+                    .HasConversion(new CnhValueConverter());
+                entity.Property(record => record.Cns)
+                    .HasConversion(new CnsValueConverter());
+                entity.Property(record => record.TituloEleitoral)
+                    .HasConversion(new TituloEleitoralValueConverter());
+                entity.Property(record => record.Nit)
+                    .HasConversion(new NitValueConverter());
+                entity.Property(record => record.PisPasep)
+                    .HasConversion(new PisPasepValueConverter());
+                entity.Property(record => record.PlacaVeiculo)
+                    .HasConversion(new PlacaVeiculoValueConverter());
+                entity.Property(record => record.Renavam)
+                    .HasConversion(new RenavamValueConverter());
+                entity.Property(record => record.Ispb)
+                    .HasConversion(new IspbValueConverter());
+                entity.Property(record => record.CodigoCompe)
+                    .HasConversion(new CodigoCompeValueConverter());
             });
 
             modelBuilder.Entity<EmailRecord>(entity =>
             {
                 entity.ToTable("EmailRecords");
                 entity.HasKey(record => record.Id);
-                entity.Property(record => record.Email).HasConversion(new EmailValueConverter());
+                entity.Property(record => record.Email)
+                    .HasConversion(new EmailValueConverter());
             });
         }
     }
