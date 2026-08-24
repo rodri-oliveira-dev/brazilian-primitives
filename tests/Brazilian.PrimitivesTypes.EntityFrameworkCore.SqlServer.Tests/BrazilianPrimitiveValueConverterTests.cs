c2808cd0c511e14be951764a79f02ca2c09a11a0
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-08-24 00:37:00 -0300

    test(efcore-sqlserver): use invariant primitive parsing

--- tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/BrazilianPrimitiveValueConverterTests.cs
+++ tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/BrazilianPrimitiveValueConverterTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Brazilian.PrimitivesTypes;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Xunit;
@@ -43,7 +44,7 @@ public sealed class BrazilianPrimitiveValueConverterTests
     [Fact]
     public void ChavePixConverterUsesCanonicalPersistenceRepresentationToResolveAmbiguousCpf()
     {
-        Cpf ambiguousCpf = Cpf.Parse("11900000083");
+        Cpf ambiguousCpf = Cpf.Parse("11900000083", CultureInfo.InvariantCulture);
         Assert.True(MobilePhone.IsValid(ambiguousCpf.Value));
 
         ChavePix original = ChavePix.From(ambiguousCpf);
@@ -67,24 +68,24 @@ public sealed class BrazilianPrimitiveValueConverterTests
 
     private static IEnumerable<ConverterCase> CreateCases()
     {
-        yield return new ConverterCase(new CpfValueConverter(), Cpf.Parse("529.982.247-25"), "52998224725", 11);
-        yield return new ConverterCase(new CnpjValueConverter(), Cnpj.Parse("00000000e08g12"), "00000000E08G12", 14);
-        yield return new ConverterCase(new CpfCnpjValueConverter(), CpfCnpj.Parse("00.000.000/e08g-12"), "00000000E08G12", 14);
-        yield return new ConverterCase(new CepValueConverter(), Cep.Parse("01311-000"), "01311000", 8);
-        yield return new ConverterCase(new EmailValueConverter(), Email.Parse("usuario@domínio.com"), "usuario@xn--domnio-5va.com", 254);
-        yield return new ConverterCase(new MobilePhoneValueConverter(), MobilePhone.Parse("(11) 98765-4321"), "11987654321", 11);
-        yield return new ConverterCase(new LandlinePhoneValueConverter(), LandlinePhone.Parse("(11) 3234-5678"), "1132345678", 10);
-        yield return new ConverterCase(new TelefoneBrasileiroValueConverter(), TelefoneBrasileiro.Parse("(11) 98765-4321"), "11987654321", 11);
-        yield return new ConverterCase(new ChavePixValueConverter(), ChavePix.From(Cpf.Parse("11900000083")), "11900000083", 77);
-        yield return new ConverterCase(new CnhValueConverter(), Cnh.Parse("02650306461"), "02650306461", 11);
-        yield return new ConverterCase(new CnsValueConverter(), Cns.Parse("123456789010000"), "123456789010000", 15);
-        yield return new ConverterCase(new TituloEleitoralValueConverter(), TituloEleitoral.Parse("000123450159"), "000123450159", 12);
-        yield return new ConverterCase(new NitValueConverter(), Nit.Parse("00000000001"), "00000000001", 11);
-        yield return new ConverterCase(new PisPasepValueConverter(), PisPasep.Parse("01234567897"), "01234567897", 11);
-        yield return new ConverterCase(new PlacaVeiculoValueConverter(), PlacaVeiculo.Parse("abc1d23"), "ABC1D23", 7);
-        yield return new ConverterCase(new RenavamValueConverter(), Renavam.Parse("00123456789"), "00123456789", 11);
-        yield return new ConverterCase(new IspbValueConverter(), Ispb.Parse("00000001"), "00000001", 8);
-        yield return new ConverterCase(new CodigoCompeValueConverter(), CodigoCompe.Parse("001"), "001", 3);
+        yield return new ConverterCase(new CpfValueConverter(), Cpf.Parse("529.982.247-25", CultureInfo.InvariantCulture), "52998224725", 11);
+        yield return new ConverterCase(new CnpjValueConverter(), Cnpj.Parse("00000000e08g12", CultureInfo.InvariantCulture), "00000000E08G12", 14);
+        yield return new ConverterCase(new CpfCnpjValueConverter(), CpfCnpj.Parse("00.000.000/e08g-12", CultureInfo.InvariantCulture), "00000000E08G12", 14);
+        yield return new ConverterCase(new CepValueConverter(), Cep.Parse("01311-000", CultureInfo.InvariantCulture), "01311000", 8);
+        yield return new ConverterCase(new EmailValueConverter(), Email.Parse("usuario@domínio.com", CultureInfo.InvariantCulture), "usuario@xn--domnio-5va.com", 254);
+        yield return new ConverterCase(new MobilePhoneValueConverter(), MobilePhone.Parse("(11) 98765-4321", CultureInfo.InvariantCulture), "11987654321", 11);
+        yield return new ConverterCase(new LandlinePhoneValueConverter(), LandlinePhone.Parse("(11) 3234-5678", CultureInfo.InvariantCulture), "1132345678", 10);
+        yield return new ConverterCase(new TelefoneBrasileiroValueConverter(), TelefoneBrasileiro.Parse("(11) 98765-4321", CultureInfo.InvariantCulture), "11987654321", 11);
+        yield return new ConverterCase(new ChavePixValueConverter(), ChavePix.From(Cpf.Parse("11900000083", CultureInfo.InvariantCulture)), "11900000083", 77);
+        yield return new ConverterCase(new CnhValueConverter(), Cnh.Parse("02650306461", CultureInfo.InvariantCulture), "02650306461", 11);
+        yield return new ConverterCase(new CnsValueConverter(), Cns.Parse("123456789010000", CultureInfo.InvariantCulture), "123456789010000", 15);
+        yield return new ConverterCase(new TituloEleitoralValueConverter(), TituloEleitoral.Parse("000123450159", CultureInfo.InvariantCulture), "000123450159", 12);
+        yield return new ConverterCase(new NitValueConverter(), Nit.Parse("00000000001", CultureInfo.InvariantCulture), "00000000001", 11);
+        yield return new ConverterCase(new PisPasepValueConverter(), PisPasep.Parse("01234567897", CultureInfo.InvariantCulture), "01234567897", 11);
+        yield return new ConverterCase(new PlacaVeiculoValueConverter(), PlacaVeiculo.Parse("abc1d23", CultureInfo.InvariantCulture), "ABC1D23", 7);
+        yield return new ConverterCase(new RenavamValueConverter(), Renavam.Parse("00123456789", CultureInfo.InvariantCulture), "00123456789", 11);
+        yield return new ConverterCase(new IspbValueConverter(), Ispb.Parse("00000001", CultureInfo.InvariantCulture), "00000001", 8);
+        yield return new ConverterCase(new CodigoCompeValueConverter(), CodigoCompe.Parse("001", CultureInfo.InvariantCulture), "001", 3);
     }
 
     private sealed record ConverterCase(
