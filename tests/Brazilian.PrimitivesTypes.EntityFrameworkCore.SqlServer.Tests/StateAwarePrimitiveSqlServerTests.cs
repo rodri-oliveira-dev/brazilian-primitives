2119767620f66802d865ec6cfe966db37adfcfee
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-08-24 04:47:11 -0300

    test: cover all persisted UF codes

--- tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/StateAwarePrimitiveSqlServerTests.cs
+++ tests/Brazilian.PrimitivesTypes.EntityFrameworkCore.SqlServer.Tests/StateAwarePrimitiveSqlServerTests.cs
@@ -17,6 +17,37 @@ public sealed class StateAwarePrimitiveSqlServerTests
         _fixture = fixture;
     }
 
+    public static TheoryData<BrazilianState, string> StateCodes => new()
+    {
+        { BrazilianState.Acre, "AC" },
+        { BrazilianState.Alagoas, "AL" },
+        { BrazilianState.Amapa, "AP" },
+        { BrazilianState.Amazonas, "AM" },
+        { BrazilianState.Bahia, "BA" },
+        { BrazilianState.Ceara, "CE" },
+        { BrazilianState.DistritoFederal, "DF" },
+        { BrazilianState.EspiritoSanto, "ES" },
+        { BrazilianState.Goias, "GO" },
+        { BrazilianState.Maranhao, "MA" },
+        { BrazilianState.MatoGrosso, "MT" },
+        { BrazilianState.MatoGrossoDoSul, "MS" },
+        { BrazilianState.MinasGerais, "MG" },
+        { BrazilianState.Para, "PA" },
+        { BrazilianState.Paraiba, "PB" },
+        { BrazilianState.Parana, "PR" },
+        { BrazilianState.Pernambuco, "PE" },
+        { BrazilianState.Piaui, "PI" },
+        { BrazilianState.RioDeJaneiro, "RJ" },
+        { BrazilianState.RioGrandeDoNorte, "RN" },
+        { BrazilianState.RioGrandeDoSul, "RS" },
+        { BrazilianState.Rondonia, "RO" },
+        { BrazilianState.Roraima, "RR" },
+        { BrazilianState.SantaCatarina, "SC" },
+        { BrazilianState.SaoPaulo, "SP" },
+        { BrazilianState.Sergipe, "SE" },
+        { BrazilianState.Tocantins, "TO" },
+    };
+
     [Fact]
     public void ContextFreeConvertersRoundTripAndRefuseToDiscardKnownState()
     {
@@ -38,14 +69,21 @@ public sealed class StateAwarePrimitiveSqlServerTests
         Assert.Throws<InvalidOperationException>(() => inscricaoConverter.ConvertToProvider(stateAwareInscricao));
     }
 
-    [Fact]
-    public void BrazilianStateConverterUsesStableUfCodesAndRejectsUnknownState()
+    [Theory]
+    [MemberData(nameof(StateCodes))]
+    public void BrazilianStateConverterRoundTripsEveryStableUfCode(BrazilianState state, string code)
     {
         BrazilianStateCodeValueConverter converter = new();
 
-        Assert.Equal("SP", converter.ConvertToProvider(BrazilianState.SaoPaulo));
-        Assert.Equal("RJ", converter.ConvertToProvider(BrazilianState.RioDeJaneiro));
-        Assert.Equal(BrazilianState.MinasGerais, converter.ConvertFromProvider("MG"));
+        Assert.Equal(code, converter.ConvertToProvider(state));
+        Assert.Equal(state, converter.ConvertFromProvider(code));
+    }
+
+    [Fact]
+    public void BrazilianStateConverterRejectsUnknownStateAndInvalidCode()
+    {
+        BrazilianStateCodeValueConverter converter = new();
+
         Assert.Throws<InvalidOperationException>(() => converter.ConvertToProvider(BrazilianState.Unknown));
         Assert.Throws<FormatException>(() => converter.ConvertFromProvider("XX"));
     }
